78fe7e9ef627e6c6c0d9dfe3ec78ddfa906d5a03
Jeff Wu <makerjeffwu@gmail.com>
2015-10-21 20:00:28 -0700

    work in progress, login.html

--- js/components/login.js
+++ js/components/login.js
@@ -1,17 +1,23 @@
 //LOGIN.JS
 
-//grab container handle
-
+//grab login button
 var login_button = document.getElementById('login_button');
 
 
-
+//==== EVENTS =====
 login_button.addEventListener('click', function() {
-  
-  var user_name = document.getElementById('user_name').value;
-
-  var password = document.getElementById('password').value;
-  
-  alert(user_name + ', ' + password);
-
+  debugFunction();
 });
+
+//FUNCTIONS
+
+function debugFunction() {
+  var user_name = document.getElementById('user_name').value;
+  var password = document.getElementById('password').value;
+  console.log(user_name + ', ' + password);
+}
+
+function grabStuff() {
+  var request = new xhrHandler();
+  
+}
