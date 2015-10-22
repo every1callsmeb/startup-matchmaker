b4c128fe2e8998bd1762310036970755ca07ffa2
harryjdixon <harryjdixon@gmail.com>
2015-10-21 18:13:47 -0700

    Sign Up Page

--- js/components/signup.js
+++ js/components/signup.js
@@ -4,6 +4,7 @@
 
 function previewFile() {
   var preview = document.querySelector('img');
+  console.log(preview)
   var file    = document.querySelector('input[type=file]').files[0];
   var reader  = new FileReader();
 
@@ -16,19 +17,4 @@ function previewFile() {
   } else {
     preview.src = "";
   }
-}
-
-
-function validate() {
-
-  var spans = document.getElementsByTagName("span")''
-  var Email = document.login.Email;
-  if(Email.value === "")
-  {
-
-      spans[0].setAttribute("style","visability:visable");
-
-  }
-
-
 }
