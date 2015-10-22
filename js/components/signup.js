a72922cb0f94663a53995dba39587aa1f1203657
robinyun <robinehyun@gmail.com>
2015-10-21 21:43:30 -0700

    Final Signup Page

--- js/components/signup.js
+++ js/components/signup.js
@@ -1,23 +1,3 @@
-
-
-//button to load photo avatar
-
-// function previewFile() {
-//   var preview = document.querySelector('img');
-//   var file    = document.querySelector('input[type=file]').files[0];
-//   var reader  = new FileReader();
-//
-//   reader.onloadend = function () {
-//     preview.src = reader.result;
-//   }
-//
-//   if (file) {
-//     reader.readAsDataURL(file);
-//   } else {
-//     preview.src = "";
-//   }
-// }
-
 var previewFile = function(event) {
   var reader = new FileReader();
   reader.onload = function(){
@@ -31,29 +11,64 @@ var xhr = new xhrHandler();
 
 var submit = document.getElementById('submit');
 submit.addEventListener('click', function(e){
-  e.preventDefault();
-  var firstName = document.getElementById('first_name').value;
-  var lastName = document.getElementById('last_name').value;
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  var email = document.getElementById('email').value;
+ e.preventDefault();
 
-  var loadElement = document.getElementById('choose_file');
+  function validateForm() {
+      var email = document.forms["myForm"]["emailaddress"].value;
+      var userid = document.forms["myForm"]["userid"].value;
+      var password = document.forms["myForm"]["password"].value;
+      var firstName = document.forms["myForm"]["firstname"].value;
+      var lastName = document.forms["myForm"]["lastname"].value;
 
-  var options = {
-    firstName: firstName,
-    lastName: lastName,
-    username: username,
-    password: password,
-    email: email,
-    avatar: {
-      image: 'signup.png'
-    }
-  };
+      if (email == null || email == "") {
+          alert("Email must be filled out");
+          return false;
+      }
+      else if (userid == null || userid == "") {
+          alert("Userid must be filled out");
+          return false;
+      }
+      else if (password == null || password == "") {
+          alert("Password must be filled out");
+          return false;
+      }
+      else if (firstName == null || firstName == "") {
+          alert("First Name must be filled out");
+          return false;
+      }
+      else if (lastName == null || lastName == "") {
+          alert("Last Name must be filled out");
+          return false;
+      }
+      else  {
+        var firstName = document.getElementById('first_name').value;
+        var lastName = document.getElementById('last_name').value;
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        var email = document.getElementById('email').value;
 
-  var request = xhr.request('POST', 'http://vvvvvv.club/api/user', options).then(function(req){
-    console.log(req);
-  });
+        var button = document.getElementById('button');
+
+        var loadElement = document.getElementById('choose_file');
+
+        var options = {
+          firstName: firstName,
+          lastName: lastName,
+          username: username,
+          password: password,
+          email: email,
+          avatar: {
+            image: 'signup.png'
+          }
+        };
+
+        var request = xhr.request('POST', 'http://vvvvvv.club/api/user', options).then(function(req){
+
+          window.location.href = ("../views/team.html");
+        });
+      }
+  }
+validateForm(); // function call for the function definition above
 });
 
 function previewFile() {
@@ -71,23 +86,3 @@ function previewFile() {
     preview.src = "";
   }
 };
-
-//function convertImgToBase64URL(url, callback, outputFormat) {
-//  var img = new Image();
-//  img.crossOrigin = 'Anonymous';
-//  img.onload = function() {
-//    var canvas = document.createElement('CANVAS');
-//    var ctx = canvas.getContext('2d');
-//    canvas.height = this.height;
-//    canvas.width = this.width;
-//    ctx.drawImage(this,0,0);
-//    var dataURL = canvas.toDataURL('image/png');
-//    callback(dataURL);
-//    canvas = null;
-//  };
-//  img.src = url;
-//}
-
-//convertImgToBase64URL('image/png', function(base64Img){
-//    options.avatar.image = base64Img;
-//});
