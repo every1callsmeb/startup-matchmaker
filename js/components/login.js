34b1c59922d5e4ba3b3175fa87fb0fa7003bd450
robinyun <robinehyun@gmail.com>
2015-10-22 19:06:09 -0700

    Commit latest updates

--- js/components/login.js
+++ js/components/login.js
@@ -6,18 +6,20 @@ var login_button = document.getElementById('login_button');
 
 //==== EVENTS =====
 login_button.addEventListener('click', function() {
+  e.preventDefault();
+
+  
   var username = document.getElementById('user_name').value;
   var password = document.getElementById('password').value;
-  
+
   var options = {
     username: username,
     password: password
   };
   //POST http://vvvvvv.club/api/login
   var xhr = new xhrHandler();
-  
+
   var request = xhr.request('POST', 'http://vvvvvv.club/api/login', options).then(function(req){console.log(req);});
 });
 
 //ERROR HANDLING
-
